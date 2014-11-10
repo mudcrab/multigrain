587f8c77cbfbda21a885b58f7325ec4de8f7278a
Jevgeni Krais <jevgeni@pitfire.eu>
2014-11-10 20:05:58 +0200

    added a dialog overlay, new dialog styling

--- app/js/views/_dialog.js
+++ app/js/views/_dialog.js
@@ -4,13 +4,33 @@ Multigrain.View = Multigrain.View || {};
 (function($) {
 	Multigrain.View.Dialog = Backbone.View.extend({
 		tagName: 'section',
-		className: 'dialog',
+		className: 'dialog-overlay',
 
 		bodyTemplate: Tpl.dialog,
+		overlayTemplate: Tpl.dialog_overlay,
+
+		initialize: function(opts)
+		{
+			this.opts = opts || {
+				title: ''
+			};
+		},
 
 		close: function()
 		{
-			this.remove();
+			var self = this;
+
+			this.$el.find('.dialog').addClass('close');
+
+			this.$el.removeClass('opened');
+			setTimeout(function() {
+				self.remove();
+			}, 300);
+		},
+
+		preRender: function()
+		{
+			// this.$el.html(this.overlayTemplate());
 		}
 
 	});
